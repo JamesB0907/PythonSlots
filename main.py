87942d0aa9d8f3a90a8a2904f082f0b4e55d6c3c
JamesB0907 <virverum22@gmail.com>
2023-10-17 10:46:11 -0400

    Making progress. Saving...

--- main.py
+++ main.py
@@ -1,6 +1,100 @@
+import random
+
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+#symbol_count is a DICTIONARY storing values (column 2) inside a corresponding key (column 1)
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    #Defining the list starting out as empty
+    all_symbols = []
+    #Loop to add symbols into list. The two variables denote the keys and the values in the dictionary, respectively
+    #".items" allows access to the key and value from the dictionary
+    for symbol, symbol_count in symbols.items():
+        #"_" is an ANNONYMOUS VARIABLE used for when you do not need iteration value. Used to prevent unused variables
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = [[], [], []]
+    for col in range(cols):
+        column = []
+        #Slice operator to create a copy of the all_symbols list
+        current_symbols = all_symbols[:]
+        for row in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+    
+        columns.append(column)
+    
+    return columns
 
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
         if amount.isdigit():
             amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than zero.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            #Checking if a value is IN BETWEEN two values
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}")
+        else:
+            print("Please enter a number.")
+
+    return amount
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        
+        if total_bet > balance:
+            print(
+                f"You do not have enough to bet that amount. You current balance is ${balance}")
+        else:
+            break
+
+    print(
+        f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
+
+main()
